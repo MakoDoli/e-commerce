438d08509779df2510d9256f9e3d78dd6e9d0739
MakoDoli <makodolidze@hotmail.com>
2025-07-12 01:02:53 +0400

    add mongoose model for user

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+  res.send("Ready to auth");
+};
+export const login = async (req, res) => {
+  res.send("Ready to login");
+};
+export const logout = async (req, res) => {
+  res.send("Ready to logout");
+};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 
 import { config } from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 config();
 
@@ -16,4 +17,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
