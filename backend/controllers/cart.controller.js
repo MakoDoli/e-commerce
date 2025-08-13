91da536e5f523113e85d359662db341faa3a2b3d
MakoDoli <makodolidze@hotmail.com>
2025-08-13 22:55:41 +0400

    add remove from cart function

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -19,6 +19,31 @@ export const addToCart = async (req, res) => {
   }
 };
 
-export const getCartProducts = async (req, res) => {};
-export const removeAllFromCart = async (req, res) => {};
+export const getCartProducts = async (req, res) => {
+  try {
+    const userId = req.user.id; // assuming this is how the user id is obtained from the request
+    const cartProducts = await Cart.find({ user: userId }).populate("product");
+    res.status(200).json(cartProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+export const removeAllFromCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+    if (!productId) {
+      user.cartItems = [];
+    } else {
+      user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+    }
+
+    await user.save();
+    res.json(user.cartItems);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "server error", error: error.message });
+  }
+};
 export const updateQuantity = async (req, res) => {};
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { addToCart } from "../controllers/cart.controller.js";
 import {
-  protectRoute,
+  addToCart,
   updateQuantity,
   removeAllFromCart,
   getCartProducts,
-} from "../middleware/auth.middleware.js";
+} from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
-router.post("/:ud", protectRoute, removeAllFromCart);
+router.delete("/:ud", protectRoute, removeAllFromCart);
 router.put("/:id", protectRoute, updateQuantity);
 
 export default router;
