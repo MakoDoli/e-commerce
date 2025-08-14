9fbd780fb4b6a89fbc79d4c4448d8bc6bd7a126e
MakoDoli <makodolidze@hotmail.com>
2025-08-14 23:38:19 +0400

    add updateQantity function

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -43,7 +43,31 @@ export const removeAllFromCart = async (req, res) => {
     res.json(user.cartItems);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "server error", error: error.message });
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 };
-export const updateQuantity = async (req, res) => {};
+export const updateQuantity = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+
+    const { quantity } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        await user.save();
+        return res.json(user.cartItems);
+      }
+      existingItem.quantity = quantity;
+      await user.save();
+      res.json(user.cartItems);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.log("Error in updateQuantity Controller", error);
+    res.status(500).json("server error");
+  }
+};
